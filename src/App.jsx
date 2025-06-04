c19c47b7c1bc4c8c1de375c71fb65cc93ad98ca2
Yashika Chaudhary <yc8545908@gmail.com>
2025-06-04 10:25:09 +0530

    Initial commit - VoxIQ base setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className="text-4xl font-bold text-pink-600">
+      Tailwind is working! 🌈
+    </div>
+  );
+}
+
+
+export default App
